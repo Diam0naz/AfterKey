33828aa0675ad74d2b4c1e2d581be4925c87fef3
Diam0naz <jutenoh@gmail.com>
2026-02-26 10:15:25 +0100

    Revert "Merge branch 'Unique-Ikeoluwa:main' into main"
    This reverts commit 22e8ac4b6efdac67aa4f5601cf59a7a229ced998, reversing
    changes made to c9404f2b1f7963cd96e01202ba5f015121cb1ac9.

--- frontend/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-12 px-6 md:px-16 bg-[#11141f] text-gray-400 text-center">
-      <p>&copy; {new Date().getFullYear()} AfterKey. All rights reserved.</p>
-      <p className="mt-2">Built on StarkNet | Powered by Web3 Technology</p>
-    </footer>
-  );
-}
